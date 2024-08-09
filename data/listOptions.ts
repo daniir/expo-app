d47259e47e791b9dd148f849f13cd5bb7ccf6ca1
daniir <danrodas03@gmail.com>
2024-08-08 22:46:38 -0500

    Lista de opciones

---
+++ data/listOptions.ts
@@ -0,0 +1,10 @@
+export const insuranceTypeList = [
+  { label: 'Terceros', value: 'Terceros' },
+  { label: 'Completo', value: 'Completo' },
+];
+
+export const coverageList = [
+  { label: 'Responsabilidad Civil ', value: 'Responsabilidad Civil ' },
+  { label: 'Limitada', value: 'Limitada' },
+  { label: 'Amplia', value: 'Amplia' },
+];
