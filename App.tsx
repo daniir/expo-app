a66e95bbb7607b47b00d4361894580e0a5d5317d
Daniel Rodas <danrodas03@gmail.com>
2024-08-11 17:14:26 -0500

    Expo App

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 export default function App() {
